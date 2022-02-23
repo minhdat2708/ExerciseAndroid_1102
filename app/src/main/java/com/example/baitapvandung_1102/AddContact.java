07a890ee7b02e91e9dcfe88bec6a8b06d5f35130
minhdat2708 <minhdat.maximus@gmail.com>
2022-02-23 17:29:38 +0700

    create menuitem

---
+++ app/src/main/java/com/example/baitapvandung_1102/AddContact.java
@@ -0,0 +1,68 @@
+package com.example.baitapvandung_1102;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class AddContact extends AppCompatActivity {
+
+    EditText edtID, edtName, edtPhone;
+    Button btnAdd, btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_contact);
+        edtID = findViewById(R.id.edtId);
+        edtName = findViewById(R.id.edtName);
+        edtPhone = findViewById(R.id.edtPhone);
+
+        btnAdd = findViewById(R.id.btnAdd);
+        btnCancel = findViewById(R.id.btnAdd);
+
+        Intent intent = getIntent();
+        Bundle bundle = intent.getExtras();
+        if (bundle!= null) {
+            int id = bundle.getInt("ID");
+            String name = bundle.getString("Name");
+            String phone = bundle.getString("Phone");
+            edtID.setText(String.valueOf(id));
+            edtName.setText(name);
+            edtPhone.setText(phone);
+            btnAdd.setText("Edit");
+        }
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Tạo Intent trở về MainActivity
+                Intent intent = new Intent();
+                //Tạo bundle là đối tượng chứa dữ liệu
+                Bundle bundle = new Bundle();
+
+                //bundle hoạt động như một Java Map các phần tử phân biệt theo key
+                //bundle có các hàng put.. trong đó ... là kiểu dữ liệu tương ứng
+                bundle.putInt("ID", Integer.parseInt(edtID.getText().toString()));
+                bundle.putString("Name", edtName.getText().toString());
+                bundle.putString("Phone", edtPhone.getText().toString());
+                //có thể đặt cả đối tượng lên bundle bằng hàm putSerilizable
+                //đặt bundle lên intent
+                intent.putExtras(bundle);
+                //trả về bằng hàm setResult
+                //tham số thứ nhất là resultCode để quản lý phiên
+                //tham số thứ hai là intent chứa dữ liệu gửi về
+                setResult(200, intent);
+                if (btnAdd.getText() == "Edit") {
+                    setResult(201, intent);
+
+                }
+                finish();
+            }
+        });
+
+    }
+}
--- app/src/main/java/com/example/baitapvandung_1102/MainActivity.java
+++ app/src/main/java/com/example/baitapvandung_1102/MainActivity.java
@@ -1,23 +1,38 @@
 package com.example.baitapvandung_1102;
 
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.SparseBooleanArray;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText edtName;
     ListView lstContact;
     ArrayList<Contacts> arrContacts;
     ArrayList<Contacts> arrSelected;
@@ -26,29 +41,96 @@ public class MainActivity extends AppCompatActivity {
     CheckBox ckbDelete;
     Button btnAdd, btnDelete;
 
-    int position = -1;
+    int positionSelected = -1;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = new MenuInflater(MainActivity.this);
+        inflater.inflate(R.menu.menuitem, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mnuSort:
+                Collections.sort(arrContacts, new NameComparator());
+                for (Contacts contacts: arrContacts) {
+                    adapter.notifyDataSetChanged();
+                }
+                Toast.makeText(MainActivity.this, "Sort", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mnuAdd:
+                //TODO
+                Intent intent = new Intent(MainActivity.this, AddContact.class);
+
+                startActivityForResult(intent, 100);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        MenuInflater inflater = new MenuInflater(MainActivity.this);
+        inflater.inflate(R.menu.contextmenuitem, menu);
+        super.onCreateContextMenu(menu, v, menuInfo);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //Lấy dữ liệu từ NewContact gửi về
+        Bundle bundle = data.getExtras();
+        int id = bundle.getInt("ID");
+        String name = bundle.getString("Name");
+        String phone = bundle.getString("Phone");
+        if (requestCode == 100 && resultCode == 200) {
+            //Đặt vào listdata
+            arrContacts.add(new Contacts(id, name, phone, false));
+            adapter.notifyDataSetChanged();
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mnuEdit:
+                //Tạo đối tượng Intent để gọi tới AddContact
+                Intent intent = new Intent(MainActivity.this, AddContact.class);
+                Bundle bundle = new Bundle();
+                bundle.putInt("ID", arrContacts.get(positionSelected).getId());
+                bundle.putString("Name", arrContacts.get(positionSelected).getName());
+                bundle.putString("Phone", arrContacts.get(positionSelected).getPhoneNumber());
+                intent.putExtras(bundle);
+                startActivityForResult(intent, 200);
+                break;
+            case R.id.mnuDelete:
+                //TODO
+                break;
+        }
+        return super.onContextItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        edtName = findViewById(R.id.editName);
         lstContact = findViewById(R.id.lsvContact);
         ckbDelete = findViewById(R.id.ckbDelete);
         btnDelete = findViewById(R.id.btnDelete);
         arrContacts = new ArrayList<>();
         arrSelected = new ArrayList<>();
 
-        arrContacts.add(new Contacts(1, "Lê Minh Đạt", "0965325125", false));
-        arrContacts.add(new Contacts(1, "Lê Minh Đạt", "0965325125", false));
-        arrContacts.add(new Contacts(1, "Lê Minh Đạt", "0965325125", false));
-        arrContacts.add(new Contacts(1, "Lê Minh Đạt", "0965325125", false));
-        arrContacts.add(new Contacts(1, "Lê Minh Đạt", "0965325125", false));
-        arrContacts.add(new Contacts(1, "Lê Minh Đạt", "0965325125", false));
-        arrContacts.add(new Contacts(1, "Lê Minh Đạt", "0965325125", false));
-        arrContacts.add(new Contacts(1, "Lê Minh Đạt", "0965325125", false));
-        arrContacts.add(new Contacts(1, "Lê Minh Đạt", "0965325125", false));
+        registerForContextMenu(lstContact);
 
+        arrContacts.add(new Contacts(1, "Lê Minh Đạt", "0965325125", false));
+        arrContacts.add(new Contacts(1, "Phạm Tiến Anh", "0965325125", false));
+        arrContacts.add(new Contacts(1, "Phạm Tiến Hải", "0965325125", false));
+        arrContacts.add(new Contacts(1, "Nguyễn Xuân Bách", "0965325125", false));
+        arrContacts.add(new Contacts(1, "Nguyễn Thị Thuỷ", "0965325125", false));
 
         adapter = new MyAdapter(MainActivity.this, arrContacts, new MyAdapter.onClick() {
             @Override
@@ -94,6 +176,34 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        edtName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int start, int before, int count) {
+                adapter.getFilter().filter(charSequence.toString());
+                adapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        lstContact.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                positionSelected = i;
+                return false;
+            }
+        });
+
+
     }
 
 //    public void onDeleteClick(Object position) {
